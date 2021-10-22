c5f22398470413476875ac2bcff15741e47879fa
Alan Costa <alancosta@powertrend.com.br>
2021-10-22 10:59:13 -0300

    Criação de rotas dinamicas via slug, template da página de places

--- src/pages/place/[slug].tsx
+++ src/pages/place/[slug].tsx
@@ -4,15 +4,14 @@
 //getServerSideProps => para buscar os dados da página (props) - runtime - toda requisição (bundle fica no server)
 //getInitialProps => para buscar os dados da página (props) - runtime - toda requisição (bundle fica no server e vai pro client) - hydrate (Está caindo em desuso)
 
-import client from '../graphql/client'
-import { GET_PAGES, GET_PAGE_BY_SLUG } from '../graphql/queries'
 import { GetStaticProps } from 'next'
+import client from 'graphql/client'
+import { GET_PLACES, GET_PLACE_BY_SLUG } from 'graphql/queries'
 import { useRouter } from 'next/dist/client/router'
-import PageTemplate, { PageTemplateProps } from 'templates/Pages'
-import { GetPageBySlugQuery, GetPagesQuery } from 'graphql/generated/graphql'
+import { GetPlacesQuery, GetPlaceBySlugQuery } from 'graphql/generated/graphql'
+import PlacesTemplate, { PlacesTemplateProps } from 'templates/Places'
 
-//Geração da página através de um template
-export default function Page({ heading, body }: PageTemplateProps) {
+export default function Place({ place }: PlacesTemplateProps) {
   const router = useRouter()
 
   //retorna um loading, qq coisa enquanto tá sendo carregado
@@ -20,14 +19,16 @@ export default function Page({ heading, body }: PageTemplateProps) {
 
   if (router.isFallback) return null
 
-  return <PageTemplate heading={heading} body={body} />
+  return <PlacesTemplate place={place} />
 }
 
 //Aqui estamos gerando as urls das páginas
 export async function getStaticPaths() {
-  const { pages } = await client.request<GetPagesQuery>(GET_PAGES, { first: 3 })
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, {
+    first: 3
+  })
 
-  const paths = pages.map(({ slug }) => ({
+  const paths = places.map(({ slug }) => ({
     params: { slug }
   }))
 
@@ -35,16 +36,18 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { page } = await client.request<GetPageBySlugQuery>(GET_PAGE_BY_SLUG, {
-    slug: `${params?.slug}`
-  })
+  const { place } = await client.request<GetPlaceBySlugQuery>(
+    GET_PLACE_BY_SLUG,
+    {
+      slug: `${params?.slug}`
+    }
+  )
 
-  if (!page) return { notFound: true }
+  if (!place) return { notFound: true }
 
   return {
     props: {
-      heading: page.heading,
-      body: page.body.html
+      place
     }
   }
 }
